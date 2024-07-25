0378eb7ed9d3418956cd2c7fbc3b6f717820f256
kelani34 <kelanitaiwo34@gmail.com>
2024-07-25 18:06:48 +0100

    update: wearable devices

--- src/WearableDevices.java
+++ src/WearableDevices.java
@@ -11,3 +11,37 @@ abstract class WearableDevice {
     // Method to retrieve activity data from the device
     public abstract Activity retrieveActivityData();
 }
+
+class Fitbit extends WearableDevice {
+    public Fitbit() {
+        super("Fitbit");
+    }
+
+    @Override
+    public void connect() {
+        System.out.println("Connecting to Fitbit...");
+    }
+
+    @Override
+    public Activity retrieveActivityData() {
+        // Dummy data
+        return new Running(1.0, 5.0, 300);
+    }
+}
+
+class AppleWatch extends WearableDevice {
+    public AppleWatch() {
+        super("Apple Watch");
+    }
+
+    @Override
+    public void connect() {
+        System.out.println("Connecting to Apple Watch...");
+    }
+
+    @Override
+    public Activity retrieveActivityData() {
+        // Dummy data
+        return new Swimming(1.0, 2.0, 250);
+    }
+}
